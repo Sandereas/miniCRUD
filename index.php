78f372830c0142b57395c3ec71610b20adf37643
Sandereas <1173194@student.roc-nijmegen.nl>
2022-03-21 15:10:38 +0100

    escape

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+ $dbhost = "localhost";
+ $dbuser = "admin";
+ $dbpass = "123";
+ $db = "minicrud";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db);
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>miniCRUDSandervdv</title>
+    <link rel="stylesheet" href="css/main.css">
+    
+
+
+
+</head>
+<style>
+
+</style>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <div class = "m2">
+
+        <?php
+        $test = "<h1>10<h1>";
+        $test2 = "<h2>8<h2>";
+        print($test);
+        echo $test2;#.$test2;
+    
+        #$Result, sqlquery,vardump,$_post
+        ?>
+        </div>
+    </div>
+    <div class="dranken">
+        <div class="cola">
+            <table>
+                <tr>
+                    Cola
+                </tr>
+                <tr>
+                    3,-
+                </tr>
+                
+            </table>
+        </div> 
+    </div>    
+    </main>
+    <footer>
+        Product created by Sandervdv
+    </footer>
+    <script src="js.main.js"></script>
+</body>
+</html>
