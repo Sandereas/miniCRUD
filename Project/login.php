0687e5379ccd332fad9e4c05ddf3c0562dd89e4e
Sandereas <1173194@student.roc-nijmegen.nl>
2022-03-27 12:49:01 +0200

    updated 12.48

--- Project/login.php
+++ Project/login.php
@@ -4,6 +4,17 @@
             <b> <h1> Editor The Web</h1> </b>
             <br>
 
+            <br>
+
+            <?php
+                if(isset($_SESSION['add']))
+                 {
+                    echo $_SESSION['add'];
+                    unset($_SESSION['add']);
+                 }
+            ?>
+            <br> <br>
+
             <a href="admin.php" class="buttonadmin"> Add Admin</a>
             <br>
             <br>
@@ -15,33 +26,53 @@
                     <th>Email</th>
                     <th>Actions</th>
                 </tr>
+
+                <?php 
+
+                    $sql = "SELECT * FROM gebruikers";
+                    $res = mysqli_query($conn, $sql);
+
+                    if($res == TRUE) 
+                    {
+                        $count = mysqli_num_rows($res);
+                    }
+                        if($count>0)
+                        {
+                            while($rows=mysqli_fetch_assoc($res))
+                             // using while loop to get all the data from the database
+
+                            {
+                                $id = $rows['id'];
+                                $gebruikersnaam = $rows['gebruikersnaam'];
+                                $email = $rows ['email'];
+                                $naam = $rows['naam'];
+
+
+                                ?>
+
                 <tr>
-                    <td>1</td>
-                    <td>Sandereas</td>
-                    <td>117194@student.roc-nijmegen.nl</td>
+                                        <td><?php echo $id ?> </td>
+                                        <td><?php echo $gebruikersnaam; ?> </td>
+                                        <td><?php echo $email; ?> </td>
+                                        <td><?php echo $naam; ?> </td>
                     <td>
                         <a href="#" class="buttonupdate"> Update admin </a> 
                         <a href="#" class="buttondelete"> Delete Admin </a> 
                     </td>
                 </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Sandervdv</td>
-                    <td>TTBSandereas@hotmail.nl</td>
-                    <td>
-                        <a href="#" class="buttonupdate"> Update admin </a> 
-                        <a href="#" class="buttondelete"> Delete Admin </a> 
-                    </td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Sander</td>
-                    <td>017194@student.roc-nijmegen.nl</td>
-                    <td>
-                        <a href="#" class="buttonupdate"> Update admin </a> 
-                        <a href="#" class="buttondelete"> Delete Admin </a> 
-                    </td>
-                </tr>
+
+                                <?php
+
+
+
+                            }
+                            
+                        }
+                        else 
+                        {
+                            
+                        }
+                ?>
             </table>
         </div>
     </main>
